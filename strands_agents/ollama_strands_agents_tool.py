595ab9421700bc2acf3591c21ef275310bf69b14
Takeshi Nishida <tnishida0309@gmail.com>
2025-06-30 07:57:55 +0900

    strands tool use add

--- strands_agents/ollama_strands_agents_tool.py
+++ strands_agents/ollama_strands_agents_tool.py
@@ -1,21 +1,24 @@
+import boto3
 from strands import Agent, tool
 from strands.models.ollama import OllamaModel
-from strands_tools import current_time, python_repl
+from strands.models import BedrockModel
+from strands_tools import current_time, python_repl, use_aws
 
 @tool
 def letter_counter(word: str, letter: str) -> int:
     return word.lower().count(letter.lower())
 
+# Ollamaモデルの例
 ollama_model = OllamaModel(
     host="http://localhost:11434",
-    # model_id="phi4-mini"
-    model_id="qwen3" 
+    model_id="phi4-mini" 
 )
 
 agent = Agent(model=ollama_model, tools=[letter_counter])
 
 message = """
-この文章の中に「1」はいくつある？
+以下の文章の中に「1」はいくつある？toolのletter_counterを使って答えてください。
+対象文章：「この文章には1が1つふくまれています。」
 """
 
 agent(message)
